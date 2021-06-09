cfde9334eecd4cfc66965ac5d484df998cb12eb3
Christian <cesium2349@protonmail.com>
2021-06-09 12:04:58 -0400

    503 cause thats a thing that happens plenty on this site LOL

---
+++ errors/503.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+    <head>
+        <title>Moobcraft</title>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Open+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/styles.css">
+    </head>
+    <body>
+	    <?php include($_SERVER['DOCUMENT_ROOT'].'/common/header.html'); ?>
+        <h2>503: Service Unavailable</h2>
+        <p>This is probably because the minecraft server is down, and stuff like analytics and the map rely on the server being online. <a href="/">let's go back home</a></p>
+    </body>
+</html>
+ 
