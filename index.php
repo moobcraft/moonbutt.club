00efeabdf05ce857c8b16cc9c49d3546d0cdbf88
Christian <cesium2349@protonmail.com>
2021-06-10 11:44:43 -0400

    we're just gonna do this

--- index.php
+++ index.php
@@ -10,19 +10,16 @@
     </head>
     <body>
 	<?php include($_SERVER['DOCUMENT_ROOT'].'/common/header.html'); ?>
-        <p>Connect with a Bedrock client to <span class=ezHighlight>moonbutt.club</span><br>
-        Port: <span class=ezHighlight>19132</span> (default)<br>
-        On Bedrock version <span class=ezHighlight>1.16.201.3</span><br>
-        <a href='rules'>Server Rules</a></p>
+        <p>Connect with a Bedrock or Java client to <span class=ezHighlight>moonbutt.club</span><br>
+        On Java version <span class=ezHighlight>1.16.5</span><br>
+        <a href='/rules'>Server Rules</a><br>
+        <a href='/dynmap/'>Server Map</a></p>
         <h2>Contact</h2>
         <p>If something goes wrong with the server, message me on any of the following. Ordered from fastest response time to slowest.</p>
         <p>Discord: <span class=ezHighlight title="230659159450845195">Cesium#5061</span><br>
         Matrix: <span class=ezHighlight>@cesium55:matrix.org</span><br>
         Email: <span class=ezHighlight>cesium2349@protonmail.com</span><br>
-        Instagram: <a class=ezHighlight href="https://instagram.com/moobcraft">@moobcraft</a><br>
-        (Instagram isn't checked at all)</p>
 	<p><a href=pgp.txt>PGP Key</a></p>
-	<p>psst... i'm doing something neat i think. check out <a href=/beta>/beta</a></p>
 	<?php include($_SERVER['DOCUMENT_ROOT'].'/common/github.html'); ?>
     </body>
 </html>
