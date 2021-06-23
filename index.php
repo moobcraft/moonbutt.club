3349fc08e6c3cbb9fab84ee922a223ed3af979a2
Christian <cesium2349@protonmail.com>
2021-06-23 19:57:50 -0400

    dead server wtf *dies of cringe*

--- index.php
+++ index.php
@@ -3,23 +3,14 @@
 <meta charset="UTF-8">
     <head>
         <title>Moobcraft</title>
-        <meta name="Description" content="Website for the Minecraft server hosted for a few friends">
+        <meta name="Description" content="Website for the Minecraft server previously hosted for a few friends">
         <meta property="og:description" content="dumb serve.r">
         <meta property="og:title" content="Moobcraft">
         <?php include($_SERVER['DOCUMENT_ROOT'].'/common/head-junk.html'); ?>
     </head>
     <body>
-	<?php include($_SERVER['DOCUMENT_ROOT'].'/common/header.php'); ?>
-        <p>Connect with a Bedrock or Java client to <span class=ezHighlight>moonbutt.club</span><br>
-        On Java version <span class=ezHighlight>1.16.5</span><br>
-        <a href='/rules'>Server Rules</a><br>
-        <a href='/dynmap/'>Server Map</a></p>
-        <h2>Contact</h2>
-        <p>If something goes wrong with the server, message me on any of the following. Ordered from fastest response time to slowest.</p>
-        <p>Discord: <span class=ezHighlight title="230659159450845195">Cesium#5061</span><br>
-        Matrix: <span class=ezHighlight>@cesium55:matrix.org</span><br>
-        Email: <span class=ezHighlight>cesium2349@protonmail.com</span><br>
-	<p><a href=pgp.txt>PGP Key</a></p>
-	<?php include($_SERVER['DOCUMENT_ROOT'].'/common/github.html'); ?>
+	    <?php include($_SERVER['DOCUMENT_ROOT'].'/common/header.php'); ?>
+        <p>For a while, people have been playing less and less on the server, and at some point i checked analytics and found that I was the only person who even joined in two months. I decided I'd leave the server alone for a bit, and then I found out the people I hosted this server for have been playing on a Realm. After seeing that people are paying to play on a server, I thought this should be the end for Moobcraft, and now I've stopped paying for the moonbutt.club domain, and stopped running the game server so I can do other things with my machine resources. When the domain expires around Feburary, this site will disappear.</p>
+	    <?php include($_SERVER['DOCUMENT_ROOT'].'/common/github.html'); ?>
     </body>
 </html>
